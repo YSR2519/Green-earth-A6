4434e26b2bed443325bb299352cbd2c476dcde1d
YSR2519 <yousufsarkar2519@gmail.com>
2025-09-13 10:10:48 +0600

    adding fetch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("ysr");
+const loadCategoris = () => {
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((json) => displayCategory(json.categories));
+};
+
+const displayCategory = (categories) => {
+    const allCategoris = document.getElementById("categoris");
+    allCategoris.innerHTML = "";
+    for(const category of categories){
+        const cataDiv = document.createElement("div");
+        cataDiv.innerHTML = `
+        <button class="btn btn-ghost hover:bg-green-500  " >${category.category_name}</button>
+        `;
+        allCategoris.append(cataDiv);
+    }
+}
+
+loadCategoris();
